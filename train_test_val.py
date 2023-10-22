9527ee3468038b1d14b15d964135587a9dd9b8b2
Stefanstud <stefan.krsteski@gmail.com>
2023-10-22 20:38:27 +0200

    initial commit

---
+++ train_test_val.py
@@ -0,0 +1,224 @@
+from sklearn.metrics import f1_score, accuracy_score, confusion_matrix
+from torch import nn
+import torch
+import numpy as np
+from utils import aggregate_edge_features_by_node
+
+
+def train(
+    model, train_loader, optimizer, criterion=nn.BCEWithLogitsLoss(), threshold=0.5
+):
+    model.train()
+    train_loss = 0.0
+
+    true_labels, preds = (
+        [],
+        [],
+    )
+
+    for batch in train_loader:
+        x, edge_index, edge_attr, labels, adj_matrix, _ = batch
+        x = x[0]
+        labels = labels[0].float()
+        adj_matrix = adj_matrix[0]
+        outputs = model(x, adj_matrix)
+        outputs = outputs.squeeze(-1)
+
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+
+        probs = torch.sigmoid(outputs)
+        predictions = (probs > threshold).float()
+
+        true_labels.extend(labels.cpu().numpy())
+        preds.extend(predictions.cpu().numpy())
+
+    train_accuracy = accuracy_score(true_labels, preds)
+    train_f1 = f1_score(true_labels, preds)
+    return train_loss / len(train_loader), train_accuracy, train_f1
+
+
+def evaluate(model, val_loader, criterion=nn.BCEWithLogitsLoss(), threshold=0.5):
+    model.eval()
+    val_loss = 0.0
+    val_labels, val_preds = [], []
+    with torch.no_grad():
+        for batch in val_loader:
+            x, edge_index, edge_attr, labels, adj_matrix, _ = batch
+
+            x = x[0]
+            labels = labels[0].float()
+            adj_matrix = adj_matrix[0]
+            outputs = model(x, adj_matrix)
+            outputs = outputs.squeeze(-1)
+
+            val_loss += criterion(outputs, labels).item()
+
+            probs = torch.sigmoid(outputs)
+            predictions = (probs > threshold).float()
+            val_labels.extend(labels.cpu().numpy())
+            val_preds.extend(predictions.cpu().numpy())
+
+    val_accuracy = accuracy_score(val_labels, val_preds)
+    val_f1 = f1_score(val_labels, val_preds)
+    return val_loss / len(val_loader), val_accuracy, val_f1
+
+
+def test(model, test_loader, criterion=nn.BCEWithLogitsLoss(), threshold=0.5):
+    model.eval()
+    test_loss = 0.0
+    test_labels, test_preds = [], []
+    with torch.no_grad():
+        for batch in test_loader:
+            x, edge_index, edge_attr, labels, adj_matrix, _ = batch
+            adj_matrix = adj_matrix[0]
+            x = x[0]
+            labels = labels[0].float()
+
+            outputs = model(x, adj_matrix)
+            outputs = outputs.squeeze(-1)
+
+            test_loss += criterion(outputs, labels).item()
+
+            probs = torch.sigmoid(outputs)
+            predictions = (probs > threshold).float()
+            test_labels.extend(labels.cpu().numpy())
+            test_preds.extend(predictions.cpu().numpy())
+
+    test_accuracy = accuracy_score(test_labels, test_preds)
+    test_f1 = f1_score(test_labels, test_preds)
+    conf_matrix = confusion_matrix(test_labels, test_preds)
+
+    return test_loss / len(test_loader), test_accuracy, test_f1, conf_matrix
+
+
+def train_edges(
+    model, train_loader, optimizer, criterion=nn.BCEWithLogitsLoss(), threshold=0.5
+):
+    model.train()
+    train_loss = 0.0
+
+    true_labels, preds = (
+        [],
+        [],
+    )
+
+    for batch in train_loader:
+        x, edge_index, edge_attr, labels, adj_matrix, adj_matrix_edges = batch
+        x = x[0]
+        labels = labels[0].float()
+        adj_matrix_edges = adj_matrix_edges[0]
+        outputs = model(x, adj_matrix_edges)
+        outputs = outputs.squeeze(-1)
+
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+
+        probs = torch.sigmoid(outputs)
+        predictions = (probs > threshold).float()
+
+        true_labels.extend(labels.cpu().numpy())
+        preds.extend(predictions.cpu().numpy())
+
+    train_accuracy = accuracy_score(true_labels, preds)
+    train_f1 = f1_score(true_labels, preds)
+    return train_loss / len(train_loader), train_accuracy, train_f1
+
+
+def evaluate_edges(model, val_loader, criterion=nn.BCEWithLogitsLoss(), threshold=0.5):
+    model.eval()
+    val_loss = 0.0
+    val_labels, val_preds = [], []
+    with torch.no_grad():
+        for batch in val_loader:
+            x, edge_index, edge_attr, labels, adj_matrix, adj_matrix_edges = batch
+
+            x = x[0]
+            labels = labels[0].float()
+            adj_matrix_edges = adj_matrix_edges[0]
+            outputs = model(x, adj_matrix_edges)
+            outputs = outputs.squeeze(-1)
+
+            val_loss += criterion(outputs, labels).item()
+
+            probs = torch.sigmoid(outputs)
+            predictions = (probs > threshold).float()
+            val_labels.extend(labels.cpu().numpy())
+            val_preds.extend(predictions.cpu().numpy())
+
+    val_accuracy = accuracy_score(val_labels, val_preds)
+    val_f1 = f1_score(val_labels, val_preds)
+    return val_loss / len(val_loader), val_accuracy, val_f1
+
+
+def test_edges(model, test_loader, criterion=nn.BCEWithLogitsLoss(), threshold=0.5):
+    model.eval()
+    test_loss = 0.0
+    test_labels, test_preds = [], []
+    with torch.no_grad():
+        for batch in test_loader:
+            x, edge_index, edge_attr, labels, adj_matrix, adj_matrix_edges = batch
+            adj_matrix_edges = adj_matrix_edges[0]
+            x = x[0]
+            labels = labels[0].float()
+
+            outputs = model(x, adj_matrix_edges)
+            outputs = outputs.squeeze(-1)
+
+            test_loss += criterion(outputs, labels).item()
+
+            probs = torch.sigmoid(outputs)
+            predictions = (probs > threshold).float()
+            test_labels.extend(labels.cpu().numpy())
+            test_preds.extend(predictions.cpu().numpy())
+
+    test_accuracy = accuracy_score(test_labels, test_preds)
+    test_f1 = f1_score(test_labels, test_preds)
+    conf_matrix = confusion_matrix(test_labels, test_preds)
+
+    return test_loss / len(test_loader), test_accuracy, test_f1, conf_matrix
+
+
+def test_add_edge_to_nodes(
+    model, test_loader, criterion=nn.BCEWithLogitsLoss(), threshold=0.5
+):
+    model.eval()
+    test_loss = 0.0
+    test_labels, test_preds = [], []
+
+    with torch.no_grad():
+        for batch in test_loader:
+            x, edge_index, edge_attr, labels, adj_matrix, _ = batch
+
+            x = x[0]
+            labels = labels[0].float()
+            adj_matrix = adj_matrix[0]
+            # aggregate the edge attributes
+            aggregated_attributes = aggregate_edge_features_by_node(
+                x, edge_index, edge_attr
+            )
+
+            # Augment or replace node features with aggregated attributes
+            # Here's an example where we concatenate them:
+            x_combined = torch.cat([x, aggregated_attributes], dim=1)
+            outputs = model(x_combined, adj_matrix)
+            outputs = outputs.squeeze(-1)
+
+            test_loss += criterion(outputs, labels).item()
+
+            probs = torch.sigmoid(outputs)
+            predictions = (probs > threshold).float()
+            test_labels.extend(labels.cpu().numpy())
+            test_preds.extend(predictions.cpu().numpy())
+
+    test_accuracy = accuracy_score(test_labels, test_preds)
+    test_f1 = f1_score(test_labels, test_preds)
+    conf_matrix = confusion_matrix(test_labels, test_preds)
+
+    return test_loss / len(test_loader), test_accuracy, test_f1, conf_matrix
---
+++ utils.py
@@ -0,0 +1,138 @@
+import networkx as nx
+from pyvis.network import Network
+import torch
+
+
+def visualize_graph_with_pyvis_node_features(dataset, graph_index, name):
+    graph_data = dataset[graph_index]
+    node_features = graph_data["node_feat"]
+    edge_index = graph_data["edge_index"]
+
+    net = Network(notebook=True, width="100%", height="50vw", cdn_resources="in_line")
+
+    for node_id, node_feat in enumerate(node_features):
+        title = f"Node {node_id}<br>Features: {node_feat}"
+        color = get_color_from_node_feature(node_feat)
+        net.add_node(node_id, title=title, color=color)
+
+    for source, target in zip(edge_index[0], edge_index[1]):
+        net.add_edge(source, target)
+
+    net.save_graph(name)
+
+
+def visualize_graph_with_pyvis_edge_features(dataset, graph_index, name):
+    graph_data = dataset[graph_index]
+    node_features = graph_data["node_feat"]
+    edge_index = graph_data["edge_index"]
+    edge_features = graph_data["edge_attr"]
+
+    net = Network(notebook=True, width="100%", height="50vw", cdn_resources="in_line")
+
+    for node_id, node_feat in enumerate(node_features):
+        title = f"Node {node_id}<br>Features: {node_feat}"
+        color = get_color_from_node_feature(node_feat)
+        net.add_node(node_id, title=title, color=color)
+
+    for edge_id, (source, target) in enumerate(zip(edge_index[0], edge_index[1])):
+        title = f"Features: {edge_features[edge_id]}"
+        color = get_color_from_edge_feature(edge_features[edge_id])
+        net.add_edge(source, target, color=color, title=title)
+
+    net.save_graph("edge_" + name)
+
+
+def get_color_from_node_feature(feature_vector):
+    colors = [
+        "#FF0000",
+        "#00FF00",
+        "#0000FF",
+        "#FFFF00",
+        "#FF00FF",
+        "#00FFFF",
+        "#000000",
+    ]
+
+    active_feature_index = feature_vector.index(1)
+
+    return colors[active_feature_index]
+
+
+def get_color_from_edge_feature(edge_vector):
+    colors = [
+        "#FF0000",
+        "#00FF00",
+        "#0000FF",
+        "#FFFF00",
+    ]
+
+    active_feature_index = edge_vector.index(1)
+
+    return colors[active_feature_index]
+
+
+def aggregate_edge_features_by_node(x, edge_index, edge_attr):
+    edge_index = edge_index.squeeze(0)
+    edge_attr = edge_attr.squeeze(0)
+
+    num_nodes = x.size(0)
+    num_features = edge_attr.size(-1)
+
+    aggregated_attributes = torch.zeros(num_nodes, num_features).to(x.device)
+
+    # iterate through each edge and sum up the edge attributes for the corresponding source node
+    for i in range(edge_index.size(1)):
+        src = edge_index[0, i].item()  #
+        aggregated_attributes[src] += edge_attr[i]
+
+    return aggregated_attributes
+
+
+def aggregate_edge_attr(x, edge_index, edge_attr):
+    edge_index = edge_index[0]
+
+    aggregated_attr = torch.full(
+        (x.size(0), edge_attr.size(1)), float("-inf"), device=x.device
+    )
+
+    for idx, (src, tgt) in enumerate(edge_index.t()):
+        aggregated_attr[tgt] = torch.max(aggregated_attr[tgt], edge_attr[idx])
+
+    aggregated_attr[aggregated_attr == float("-inf")] = 0
+
+    return aggregated_attr
+
+
+# def find_threshold(model, val_loader, edges=False):
+#     model.eval()
+#     true_labels = []
+#     probs_list = []
+
+#     for batch in val_loader:
+#         x, edge_index, edge_attr, labels, adj_matrix, adj_matrix_edges = batch
+#         x = x[0]
+#         labels = labels[0].float()
+
+#         if edges:
+#             adj_matrix = adj_matrix_edges[0]
+#         else:
+#             adj_matrix = adj_matrix[0]  # Use the node adjacency matrix
+
+#         with torch.no_grad():
+#             outputs = model(x, adj_matrix)
+#         probs = torch.sigmoid(outputs)
+#         true_labels.extend(labels.cpu().numpy())
+#         probs_list.extend(probs.cpu().numpy())
+
+#     thresholds = np.arange(0.1, 1, 0.01)
+#     best_threshold = 0.5
+#     best_f1 = 0.0
+
+#     for threshold in thresholds:
+#         predictions = (probs_list > threshold).astype(float)
+#         f1 = f1_score(true_labels, predictions)
+#         if f1 > best_f1:
+#             best_f1 = f1
+#             best_threshold = threshold
+
+#     return best_threshold
